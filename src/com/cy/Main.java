5dbe88d015db208b9d0941ef68285ff4cdce5b7e
lidehua <lidehua@.com>
2022-06-09 18:02:52 +0800

    hot-fix second commit

--- src/com/cy/Main.java
+++ src/com/cy/Main.java
@@ -1,9 +1,12 @@
 package com.cy;
 
+import com.sun.media.sound.SoftTuning;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("钟宇");
+        System.out.println("幺儿");
     }
 }
